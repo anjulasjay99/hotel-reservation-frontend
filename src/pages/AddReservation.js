338d8b598a223b19312ae697c4ab07e1e9cd5046
SKShehan <shehansilva2013@gmail.com>
2022-05-10 21:06:09 +0530

    Add Reservation
    Add Reservation Completed and Axios module added

--- src/pages/AddReservation.js
+++ src/pages/AddReservation.js
@@ -1,3 +1,6 @@
+/* eslint-disable prefer-const */
+/* eslint-disable no-else-return */
+/* eslint-disable no-unused-vars */
 import { useState } from "react";
 // @mui material components
 import Container from "@mui/material/Container";
@@ -9,11 +12,97 @@ import MKInput from "components/MKInput";
 import MKButton from "components/MKButton";
 import MKTypography from "components/MKTypography";
 
+import axios from 'axios';
+
 function AddReservationHotel() {
   const [checked, setChecked] = useState(true);
+  const [firstName , setFname] = useState("");
+  const [lastName , setLname] = useState("");
+  const [email , setEmail] = useState("");
+  const [telNo , setTelno] = useState("");
+  const [country , setCountry] = useState("");
+  const [checkInDate , setCheckIn] = useState("");
+  const [checkOutDate , setCheckOut] = useState("");
+  const [room , setRoom] = useState("");
+  const [noOfChildren , setChidren] = useState(0);
+  const [noOfAdults , setAdults] = useState(0);
+  const [totalPayment , setTotalPayment] = useState("");
+  const [priceA , setPriceA] = useState("");
+  const [priceC , setPriceC] = useState(""); 
+  
+  const hotel = "Rivers Edge Nature Resorts";
 
+  const rooms = [
+    {
+      id: "1",
+      title: "Deluxe King Size",
+      priceA: "25,000",
+      priceC: "10,000"
+    },
+    {
+      id: "2",
+      title: "King Size Sleigh Bed",
+      priceA: "35,000",
+      priceC: "15,000"
+    },
+    {
+      id: "3",
+      title: "Compact Double",
+      priceA: "45,000",
+      priceC: "20,000"
+    },
+    {
+      id: "4",
+      title: "Deluxe Twin/Large Double",
+      priceA: "45,000",
+      priceC: "20,000"
+    },
+    {
+      id: "5",
+      title: "King Size Four Poster",
+      priceA: "45,000",
+      priceC: "20,000"
+    },
+  ];
   const handleChecked = () => setChecked(!checked);
 
+  function CalculatePayment (SelectedRoom){
+      rooms.forEach((r) =>{
+        if(r.title === SelectedRoom){
+          setPriceA(r.priceA);
+          setPriceC(r.priceC);
+        }
+        
+          
+      });
+      setTotalPayment(priceA * noOfAdults + priceC * noOfChildren)
+  }
+
+  function AddReservationClick(e) {
+    e.preventDefault();
+
+    const newReservation = {
+      firstName,
+      lastName,
+      email,
+      telNo,
+      country,
+      checkInDate,
+      checkOutDate,
+      noOfChildren,
+      noOfAdults,
+      room,
+      hotel,
+      totalPayment
+    }
+
+    axios.post("http://localhost:8070/reservation/save" , newReservation ).then(()=>{
+        e.target.reset();
+      }).catch((err)=>{
+          alert(err);
+     });
+
+  }
   return (
     <MKBox component="section" py={12}>
       <Container>
@@ -27,37 +116,70 @@ function AddReservationHotel() {
             <MKBox p={3}>
               <Grid container spacing={3}>
                 <Grid item xs={12} md={6}>
-                  <MKInput variant="standard" label="First Name" fullWidth />
+                  <MKInput variant="standard" name = "Fname" value = {firstName} label="First Name" fullWidth
+                  onChange = {(e) =>{
+                    setFname(e.target.value);
+                  }} />
                 </Grid>
                 <Grid item xs={12} md={6}>
-                  <MKInput variant="standard" label="Last Name" fullWidth />
+                  <MKInput variant="standard" name = "Lname" value = {lastName} label="Last Name" fullWidth 
+                  onChange = {(e) =>{
+                    setLname(e.target.value);
+                  }}                  />
                 </Grid>
                 <Grid item xs={12}>
-                  <MKInput variant="standard" type="email" label="Email Address" fullWidth />
+                  <MKInput variant="standard" type="email" name = "email" value = {email} label="Email Address" fullWidth 
+                  onChange = {(e) =>{
+                    setEmail(e.target.value);
+                  }}                  />
                 </Grid>
                 <Grid item xs={12}>
-                  <MKInput variant="standard" label="Telephone Number" fullWidth />
+                  <MKInput variant="standard" name = "telNo" value = {telNo} label="Telephone Number" fullWidth 
+                  onChange = {(e) =>{
+                    setTelno(e.target.value);
+                  }}                  />
                 </Grid>
                 <Grid item xs={12}>
-                  <MKInput variant="standard" label="Country" fullWidth />
+                  <MKInput variant="standard" name = "country" value = {country} label="Country" fullWidth 
+                  onChange = {(e) =>{
+                    setCountry(e.target.value);
+                  }}                  />
                 </Grid>
                 <Grid item xs={12} md={6}>
-                  <MKInput type="date" variant="standard" label="Check in Date" fullWidth />
+                  <MKInput type="date" variant="standard" name = "CheckIn" value = {checkInDate} label="Check in Date" fullWidth 
+                  onChange = {(e) =>{
+                    setCheckIn(e.target.value);
+                  }}                  />
                 </Grid>
                 <Grid item xs={12} md={6}>
-                  <MKInput type="date" variant="standard" label="Check out Date" fullWidth />
+                  <MKInput type="date" variant="standard" name = "CheckOut" value = {checkOutDate} label="Check out Date" fullWidth 
+                  onChange = {(e) =>{
+                    setCheckOut(e.target.value);
+                  }}                  />
                 </Grid>
                 <Grid item xs={12}>
-                  <MKInput variant="standard" label="Room" fullWidth />
+                  <MKInput variant="standard" name = "Room" value = {room} label="Room" fullWidth 
+                  onChange = {(e) =>{
+                    setRoom(e.target.value);
+                  }}                  />
                 </Grid>
                 <Grid item xs={12} md={6}>
-                  <MKInput variant="standard" label="Number of Children" fullWidth />
+                  <MKInput variant="standard" name = "ChildrenN" value = {noOfChildren} label="Number of Children" fullWidth 
+                  onChange = {(e) =>{
+                    setChidren(e.target.value);
+                  }}                  />
                 </Grid>
                 <Grid item xs={12} md={6}>
-                  <MKInput variant="standard" label="Number of Adults" fullWidth />
+                  <MKInput variant="standard" name = "AdultsN" value = {noOfAdults} label="Number of Adults" fullWidth 
+                  onChange = {(e) =>{
+                    setAdults(e.target.value);
+                  }}                  />
                 </Grid>
                 <Grid item xs={12}>
-                  <MKInput variant="standard" label="Total Payment" fullWidth />
+                  <MKInput variant="standard" name = "TotalPayment" value = {totalPayment} label="Total Payment" fullWidth 
+                  onClick = {(e) =>{
+                    CalculatePayment({room});
+                  }}  disabled          />
                 </Grid>
                 <Grid item xs={12} alignItems="center" ml={-1}>
                   <Switch checked={checked} onChange={handleChecked} />
