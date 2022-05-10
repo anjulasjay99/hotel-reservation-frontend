48747243eac0eac85d38fdadb1eb77793db25246
SKShehan <shehansilva2013@gmail.com>
2022-05-11 00:30:23 +0530

    Add and View
    Add and View reservations Hotel completed

--- src/pages/AddReservation.js
+++ src/pages/AddReservation.js
@@ -26,7 +26,7 @@ function AddReservationHotel() {
   const [room , setRoom] = useState("");
   const [noOfChildren , setChidren] = useState(0);
   const [noOfAdults , setAdults] = useState(0);
-  const [totalPayment , setTotalPayment] = useState("");
+  const [totalPayment , setTotalPayment] = useState(0);
   const [priceA , setPriceA] = useState("");
   const [priceC , setPriceC] = useState(""); 
   
@@ -36,51 +36,62 @@ function AddReservationHotel() {
     {
       id: "1",
       title: "Deluxe King Size",
-      priceA: "25,000",
-      priceC: "10,000"
+      priceA: 25000,
+      priceC: 10000
     },
     {
       id: "2",
       title: "King Size Sleigh Bed",
-      priceA: "35,000",
-      priceC: "15,000"
+      priceA: 35000,
+      priceC: 15000
     },
     {
       id: "3",
       title: "Compact Double",
-      priceA: "45,000",
-      priceC: "20,000"
+      priceA: 45000,
+      priceC: 20000
     },
     {
       id: "4",
       title: "Deluxe Twin/Large Double",
-      priceA: "45,000",
-      priceC: "20,000"
+      priceA: 45000,
+      priceC: 20000
     },
     {
       id: "5",
       title: "King Size Four Poster",
-      priceA: "45,000",
-      priceC: "20,000"
+      priceA: 45000,
+      priceC: 20000
     },
   ];
-  const handleChecked = () => setChecked(!checked);
+
 
   function CalculatePayment (SelectedRoom){
+    console.log("Payment");
+    console.log(SelectedRoom);
       rooms.forEach((r) =>{
+        
         if(r.title === SelectedRoom){
           setPriceA(r.priceA);
           setPriceC(r.priceC);
+          console.log(r.title);
+          console.log(r.priceA);
+          console.log(r.priceC);
         }
         
           
       });
-      setTotalPayment(priceA * noOfAdults + priceC * noOfChildren)
+      console.log(noOfAdults );
+      console.log(noOfChildren );
+      let tot = priceA * noOfAdults + priceC * noOfChildren;
+      console.log(tot);
+      setTotalPayment(tot);
+      console.log(totalPayment);
   }
 
   function AddReservationClick(e) {
+   
     e.preventDefault();
-
     const newReservation = {
       firstName,
       lastName,
@@ -97,7 +108,7 @@ function AddReservationHotel() {
     }
 
     axios.post("http://localhost:8070/reservation/save" , newReservation ).then(()=>{
-        e.target.reset();
+        alert("Reservation added");
       }).catch((err)=>{
           alert(err);
      });
@@ -108,7 +119,7 @@ function AddReservationHotel() {
       <Container>
         <Grid container item justifyContent="center" xs={10} lg={7} mx="auto" textAlign="center">
           <MKTypography variant="h3" mb={1}>
-            Contact Us
+            Room Reservation
           </MKTypography>
         </Grid>
         <Grid container item xs={12} lg={7} sx={{ mx: "auto" }}>
@@ -161,51 +172,39 @@ function AddReservationHotel() {
                   <MKInput variant="standard" name = "Room" value = {room} label="Room" fullWidth 
                   onChange = {(e) =>{
                     setRoom(e.target.value);
+                    CalculatePayment(room);
                   }}                  />
                 </Grid>
                 <Grid item xs={12} md={6}>
                   <MKInput variant="standard" name = "ChildrenN" value = {noOfChildren} label="Number of Children" fullWidth 
                   onChange = {(e) =>{
                     setChidren(e.target.value);
+                    CalculatePayment(room);
                   }}                  />
+                  {/* <input type="number" min="1" max = "10" onChange = {(e) =>{
+                    setChidren(e.target.value);
+                    CalculatePayment(room);
+                  }}/> */}
                 </Grid>
                 <Grid item xs={12} md={6}>
                   <MKInput variant="standard" name = "AdultsN" value = {noOfAdults} label="Number of Adults" fullWidth 
                   onChange = {(e) =>{
                     setAdults(e.target.value);
+                    CalculatePayment(room);
                   }}                  />
+                {/* <input type="number" min="1" max = "10" onChange = {(e) =>{
+                    setAdults(e.target.value);
+                    CalculatePayment(room);
+                  }}/> */}
                 </Grid>
                 <Grid item xs={12}>
-                  <MKInput variant="standard" name = "TotalPayment" value = {totalPayment} label="Total Payment" fullWidth 
-                  onClick = {(e) =>{
-                    CalculatePayment({room});
-                  }}  disabled          />
-                </Grid>
-                <Grid item xs={12} alignItems="center" ml={-1}>
-                  <Switch checked={checked} onChange={handleChecked} />
-                  <MKTypography
-                    variant="button"
-                    fontWeight="regular"
-                    color="text"
-                    ml={-1}
-                    sx={{ cursor: "pointer", userSelect: "none" }}
-                    onClick={handleChecked}
-                  >
-                    &nbsp;&nbsp;I agree the&nbsp;
-                  </MKTypography>
-                  <MKTypography
-                    component="a"
-                    href="#"
-                    variant="button"
-                    fontWeight="regular"
-                    color="dark"
-                  >
-                    Terms and Conditions
-                  </MKTypography>
+                  <MKInput variant="standard" name = "TotalPayment" value = {totalPayment} label="Total Payment" fullWidth  disabled          />
                 </Grid>
               </Grid>
               <Grid container item justifyContent="center" xs={12} my={2}>
-                <MKButton type="submit" variant="gradient" color="dark" fullWidth>
+                <MKButton type="submit" variant="gradient" color="dark" fullWidth onClick = {(event) =>{
+                  AddReservationClick(event);
+                }}>
                   Add Reservation
                 </MKButton>
               </Grid>
