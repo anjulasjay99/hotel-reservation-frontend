d7a350ec243737ae6a6bbe6c6eb9bf4531073276
IT19967080 <it19967080@my.sliit.lk>
2022-05-11 17:10:44 +0530

    Add registration page

--- src/App.js
+++ src/App.js
@@ -40,6 +40,7 @@ import PaymentUnsuccessful from "pages/PaymentUnsuccessful";
 import RoomsView from "pages/Rooms";
 import MapContainer from "pages/MapContainer";
 import UpdateReservation from "pages/updateReservation";
+import Registration from "pages/Registration";
 
 export default function App() {
   const { pathname } = useLocation();
@@ -87,6 +88,7 @@ export default function App() {
         <Route path="/view-rooms" element={<RoomsView />} />
         <Route path="/view-location" element={<MapContainer />} />
         <Route path="/update" element={<UpdateReservation />} />
+        <Route path="/register" element={<Registration />} />
       </Routes>
     </ThemeProvider>
   );
